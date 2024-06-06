32b47e26f3443638afcd5f81ce93491e58627d19
VulDoctor <15088615425@163.com>
2024-06-06 09:53:38 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,10 +1,15 @@
 python VulDoctor.py \
-    --output_dir=./saved_models \
     --model_name=model.bin \
+    --output_dir=./saved_models \
     --tokenizer_name=Salesforce/codet5-base \
     --model_name_or_path=Salesforce/codet5-base \
-    --do_test \
+    --do_train \
+    --epochs 75 \
     --encoder_block_size 512 \
     --decoder_block_size 256 \
-    --num_beams=50 \
-    --eval_batch_size 1
+    --train_batch_size 8 \
+    --eval_batch_size 8 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456  2>&1 | tee train.log
